61d5a6f0d23e7af7c315b10dbd8cbe118d469288
Arief Maulana <ariefmaulana0808@gmail.com>
2021-12-16 09:25:39 +0700

    Initial Commit

--- src/components/CharityList.svelte
+++ src/components/CharityList.svelte
@@ -1,11 +1,128 @@
 <script>
-    export let charities = ['Default charity 1'];
+    export let charities
 </script>
-<div>
+
+<!-- popularCauses section -->
+<section id="popularcause" class="bg-gray waypoint-tigger xs-section-padding">
+    <div class="container">
+        <div class="xs-heading row xs-mb-60">
+            <div class="col-md-9 col-xl-9">
+                <h2 class="xs-title">Popular Causes</h2>
+                <span class="xs-separetor dashed"></span>
+                <p>FundPress has built a platform focused on aiding entrepreneurs, startups, and <br> companies
+                    raise capital from anyone.</p>
+            </div><!-- .xs-heading-title END -->
+        </div><!-- .row end -->
+        {#if charities !== undefined}
+        {#each charities as charity}
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <!-- modal goes here -->
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Splash Drone 3 a Fully Waterproof
+                                    Drone that
+                                    floats</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="exampleInputAmount">Amount donation</label>
+                                        <input required type="number" class="form-control" id="exampleInputAmount"
+                                            aria-describedby="amountHelp" placeholder="Enter amount">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputName">Your name</label>
+                                        <input required type="text" class="form-control" id="exampleInputName"
+                                            aria-describedby="nameHelp" placeholder="Enter full name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address</label>
+                                        <input required type="email" class="form-control" id="exampleInputEmail1"
+                                            aria-describedby="emailHelp" placeholder="Enter email">
+                                    </div>
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1">I Agree</label>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-primary">Continue</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="xs-popular-item xs-box-shadow">
+                    <div class="xs-item-header">
+
+                        <img src="{charity.thumbnail}" alt="">
+
+                        <div class="xs-skill-bar">
+                            <div class="xs-skill-track">
+                                <p><span class="number-percentage-count number-percentage" data-value="90"
+                                        data-animation-duration="3500">{charity.pledged}</span>%</p>
+                            </div>
+                        </div>
+                    </div><!-- .xs-item-header END -->
+                    <div class="xs-item-content">
+                        <ul class="xs-simple-tag xs-mb-20">
+                            <li><a href="">{charity.category}</a></li>
+                        </ul>
+
+                        <a href="#" class="xs-post-title xs-mb-30">
+                            {charity.title}
+                        </a>
+
+                        <ul class="xs-list-with-content">
+                            <li>{charity.pledged}<span>Pledged</span></li>
+                            <li><span class="number-percentage-count number-percentage" data-value="90"
+                                    data-animation-duration="3500">{charity.pledged}</span>% <span>Funded</span></li>
+                            <li>{charity.date_end}<span>Days to go</span></li>
+                        </ul>
+
+                        <span class="xs-separetor"></span>
+
+                        <div class="row xs-margin-0">
+                            <div class="xs-round-avatar">
+                                <img src="{charity.profile_photo}" alt="">
+                            </div>
+                            <div class="xs-avatar-title">
+                                <a href="#"><span>By</span>{charity.profile_name}</a>
+                            </div>
+                        </div>
+
+                        <span class="xs-separetor"></span>
+
+                        <a href="#" data-toggle="modal" data-target="#exampleModal"
+                            class="btn btn-primary btn-block">
+                            Donate This Cause
+                        </a>
+                    </div><!-- .xs-item-content END -->
+                </div><!-- .xs-popular-item END -->
+            </div>
+        </div><!-- .row end -->
+        {/each}
+        {/if}
+    </div><!-- .container end -->
+</section><!-- End popularCauses section -->
+
+<!-- <div>
     <h2>Daftar Charity</h2>
-    <pre>{charities}</pre>
-    <ul>
-        <li>Charity 1</li>
-        <li>Charity 2</li>
-    </ul>
-</div>
+    {#if charities !== undefined}
+        <ul>
+            {#each charities as charity}
+                 <li>{charity.title} - </li>
+            {/each}
+        </ul>
+    {:else}
+    <h5>Data Doesn't Exist</h5>
+    {/if}
+</div> -->
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,21 +1,15 @@
 <script>
 	import CharityList from '../components/CharityList.svelte';
+	import Header from '../components/Header.svelte';
+	import Welcome from '../components/Welcome.svelte';
+	import Promo from '../components/Promo.svelte';
+	import Footer from '../components/Footer.svelte'
+	import { charities } from '../data/charities';
 	let title = "Charity";
-	let charities = ['Charity 1', 'Charity 2', 'Charity 3']
 </script>
 
-<style>
-	h1 {
-		font-size: 3em;
-		color: #e2071c;
-	}
-</style>
-
-<svelte:head>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kimeiga/bahunya@css/bahunya-0.1.3.css">
-</svelte:head>
-
-<div>
-	<h1>{title}</h1>
-	<CharityList {charities} />
-</div>
+<Header />
+<Welcome />
+<CharityList {charities} />
+<Promo />
+<Footer />
